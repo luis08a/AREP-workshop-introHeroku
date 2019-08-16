69c24e2ef46da7186aa1a630e4bfacf168a1942a
luis <2135429@labinfo.is.escuelaing.edu.co>
2019-08-16 16:51:00 -0500

    Finished

--- src/main/java/edu/escuelaing/arem/App.java
+++ src/main/java/edu/escuelaing/arem/App.java
@@ -6,9 +6,18 @@ import static spark.Spark.*;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+
     public static void main(String[] args) {
+        port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
+        
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; // returns default port if heroku-port isn't set (i.e. on localhost)
     }
 }
